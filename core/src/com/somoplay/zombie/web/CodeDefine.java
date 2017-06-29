f4346e6ce4b5462306b726c0f8fc82794a62e388
snowback <bback0724@gmail.com>
2017-06-29 11:48:30 -0400

    [Update] Add a definition file

---
+++ core/src/com/somoplay/zombie/web/CodeDefine.java
@@ -0,0 +1,15 @@
+package com.somoplay.zombie.web;
+
+/**
+ * Created by shoong on 2017-06-28.
+ */
+
+public class CodeDefine {
+    public static final int CODE_MSG_ANSWER_LOGIN = 10001;
+    public static final int CODE_MSG_ADD_PLAYER = 10002;
+    public static final int CODE_MSG_REMOVE_PLAYER = 10003;
+    public static final int CODE_MSG = 10004;
+    public static final int MSG_PLAYER_MOVING = 3000;
+    public static final int MSG_PLAYER_SHOOTING = 3001;
+    public static final int MSG_KILLED_MONSTER = 3002;
+}
